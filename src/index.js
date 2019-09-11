e8674b9c630821f25fee3f12e49fb1624b108053
John <jothachil@gmail.com>
2019-09-11 23:00:07 +0530

    Added Emoji

--- src/index.js
+++ src/index.js
@@ -12,36 +12,36 @@ import {
 function layer(context, layer) {
     switch (JSON.stringify(layer.shadows)) {
         case JSON.stringify(dp1):
-            return "Shadow Elevation : dp1"
+            return " 🔆Shadow Elevation : dp1"
             break;
         case JSON.stringify(dp2):
-            return "Shadow Elevation : dp2"
+            return " 🔆Shadow Elevation : dp2"
             break;
         case JSON.stringify(dp4):
-            return "Shadow Elevation : dp4"
+            return " 🔆Shadow Elevation : dp4"
             break;
         case JSON.stringify(dp6):
-            return "Shadow Elevation : dp6"
+            return " 🔆Shadow Elevation : dp6"
             break;
         case JSON.stringify(dp8):
-             return "Shadow Elevation : dp8"
+             return " 🔆Shadow Elevation : dp8"
             break;
         case JSON.stringify(dp12):
-            return "Shadow Elevation : dp12"
+            return " 🔆Shadow Elevation : dp12"
             break;    
         case JSON.stringify(dp16):
-            return "Shadow Elevation : dp16"
+            return " 🔆Shadow Elevation : dp16"
             break;
         case JSON.stringify(dp24):
-            return "Shadow Elevation : dp24"
+            return " 🔆Shadow Elevation : dp24"
             break;
         default:
             if(layer.shadows.length == 0 ){
-                return "No Shadow on this layer "
+                return "No 🔆Shadow on this layer "
             }
             else
             {
-                return "Shadow not style guide.\nContact the design owner"
+                return "Looks like someone used wrong shadow👀"
             }
             
     }
